32121cee82964963bc9ec50a30830c3b3875319f
Igor Safonov <igorsafonov42@gmail.com>
2021-11-05 21:54:22 -0700

    bot: add object context to commands

---
+++ src/bot.py
@@ -0,0 +1,159 @@
+from telegram.ext import Updater, CommandHandler
+from TOKEN import TOKEN
+
+import logging
+
+
+class bot:
+    def __init__(self):
+        # Create bot
+        self.updater = Updater(token=TOKEN, use_context=True)
+        d = self.updater.dispatcher
+
+        # Register API
+        d.add_handler(CommandHandler('help', self.help))
+        d.add_handler(CommandHandler('register', self.register))
+        d.add_handler(CommandHandler('join', self.join))
+        d.add_handler(CommandHandler('ack', self.ack))
+        d.add_handler(CommandHandler('nack', self.nack))
+        d.add_handler(CommandHandler('stat', self.stat))
+        d.add_handler(CommandHandler('log', self.log))
+        d.add_handler(CommandHandler('payment', self.payment))
+        d.add_handler(CommandHandler('g_add', self.g_add))
+        d.add_handler(CommandHandler('add', self.add))
+        d.add_handler(CommandHandler('cancel', self.cancel))
+        d.add_handler(CommandHandler('pay', self.pay))
+        d.add_handler(CommandHandler('reset', self.reset))
+
+    def run(self):
+        self.updater.start_polling()
+
+    def log_info(func):
+        def log(self, update, context):
+            logging.info(f'Received command: {update.message.text}')
+            return func(self, update, context)
+        return log
+
+    def __reply_unimpl(self, update):
+        update.message.reply_text('Unimplemented command')
+
+    def __reply_invalid(self, update):
+        update.message.reply_text('Invalid command')
+
+    def __get_sender_id(update):
+        sender = update.message.from_user.id
+
+    def __get_mentioned_ids(update):
+        msg = update.message
+        entities = msg.entities
+        mentioned_ids = []
+
+        for e in entities:
+            if e.type == 'mention':
+                user_begin = e.offset
+                user_end = user_begin + e.length
+                username = msg.text[user_begin:user_end]
+                # TODO: convert username to user_id
+                username_id = username
+                mentioned_ids.append(username_id)
+        return mentioned_ids
+
+    # Bot API
+    @log_info
+    def help(self, update, context):
+        # TODO: generate help
+        self.__reply_unimpl(update)
+
+    @log_info
+    def register(self, update, context):
+        sender_id = __get_sender_id(update)
+        # TODO: make a call to Backend with sender_id
+        __reply_unimpl(update)
+
+    @log_info
+    def join(self, update, context):
+        sender_id = __get_sender_id(update)
+        mentioned_ids = __get_mentioned_ids(update)
+        if len(mentioned_ids) != 1:
+            __reply_invalid(update)
+    #        __reply_help('join')
+            return
+
+        # TODO: make a call to Backend with: sender_id, mentioned_ids
+        __reply_unimpl(update)
+
+    @log_info
+    def ack(self, update, context):
+        sender_id = __get_sender_id(update)
+        # TODO: make a call to Backend with: sender_id
+        __reply_unimpl(update)
+
+    @log_info
+    def nack(self, update, context):
+        sender_id = __get_sender_id(update)
+        # TODO: make a call to Backend with: sender_id
+        __reply_unimpl(update)
+
+    @log_info
+    def stat(self, update, context):
+        sender_id = __get_sender_id(update)
+        # TODO: make a call to Backend with: sender_id
+        __reply_unimpl(update)
+
+    @log_info
+    def log(self, update, context):
+        sender_id = __get_sender_id(update)
+        words = update.message.text.split(' ')
+        print(words)
+        num_tx = 0
+        if len(words) > 1:
+            num_tx = int(words[1])
+        # TODO: make a call to Backend with: sender_id, num_tx
+        __reply_unimpl(update)
+
+    @log_info
+    def payment(self, update, context):
+        sender_id = __get_sender_id(update)
+        # TODO: make a call to Backend with: sender_id
+        __reply_unimpl(update)
+
+    @log_info
+    def g_add(self, update, context):
+        sender_id = __get_sender_id(update)
+        mentioned_ids = __get_mentioned_ids(update)
+        # TODO: make a call to Backend with: sender_id, mentioned_ids
+        __reply_unimpl(update)
+
+    @log_info
+    def add(self, update, context):
+        sender_id = __get_sender_id(update)
+        mentioned_ids = __get_mentioned_ids(update)
+        # TODO: make a call to Backend with: sender_id, mentioned_ids
+        __reply_unimpl(update)
+
+    @log_info
+    def cancel(self, update, context):
+        sender_id = __get_sender_id(update)
+        words = update.message.text.split(' ')
+        if len(words) < 2:
+            __reply_invalid(update)
+            return
+        tx = words[1]
+        comment = ''
+        if len(words) > 2:
+            comment = words[2]
+        # TODO: make a call to Backend with: sender_id, tx, comment
+        __reply_unimpl(update)
+
+    @log_info
+    def pay(self, update, context):
+        sender_id = __get_sender_id(update)
+        mentioned_ids = __get_mentioned_ids(update)
+        # TODO: make a call to Backend with: sender_id, mentioned_ids
+        __reply_unimpl(update)
+
+    @log_info
+    def reset(self, update, context):
+        sender_id = __get_sender_id(update)
+        # TODO: make a call to Backend with: sender_id
+        __reply_unimpl(update)
--- src/frontend.py
+++ src/frontend.py
@@ -1,32 +1,17 @@
-from telegram.ext import Updater, CommandHandler
-from TOKEN import TOKEN
-
 import logging
-import commands
+
+import bot
 
 
-def init_bot():
-    # Create bot
-    updater = Updater(token=TOKEN, use_context=True)
-    d = updater.dispatcher
+class Collector:
+    def __init__(self):
+        # data_base = DB()
+        # data_base.open("collector_test")
+        # self.backend = TFinance(data_base)
+        self.bot = bot.bot()
 
-    # Register API
-    d.add_handler(CommandHandler('help', commands.help))
-    d.add_handler(CommandHandler('register', commands.register))
-    d.add_handler(CommandHandler('join', commands.join))
-    d.add_handler(CommandHandler('ack', commands.ack))
-    d.add_handler(CommandHandler('nack', commands.nack))
-    d.add_handler(CommandHandler('stat', commands.stat))
-    d.add_handler(CommandHandler('log', commands.log))
-    d.add_handler(CommandHandler('payment', commands.payment))
-    d.add_handler(CommandHandler('g_add', commands.g_add))
-    d.add_handler(CommandHandler('add', commands.add))
-    d.add_handler(CommandHandler('cancel', commands.cancel))
-    d.add_handler(CommandHandler('pay', commands.pay))
-    d.add_handler(CommandHandler('reset', commands.reset))
-
-    # Run bot
-    updater.start_polling()
+    def run(self):
+        self.bot.run()
 
 
 def main():
@@ -34,10 +19,8 @@ def main():
                         '- %(levelname)s - %(message)s',
                         level=logging.INFO)
 
-    # TODO: Connect to Backend
-
-    # Init bot
-    init_bot()
+    collector = Collector()
+    collector.run()
 
 
 if __name__ == '__main__':
