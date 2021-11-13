834088cec084dd78923f9920a0b67024decce4ec
evatux <evatux@gmail.com>
2021-11-12 21:56:29 -0800

    fixup: src: be: umath: implement payment

--- src/backend/test/test_umath.py
+++ src/backend/test/test_umath.py
@@ -8,17 +8,24 @@ SRC_DIR = os.path.dirname(os.path.dirname(TEST_DIR))
 sys.path.insert(0, SRC_DIR)
 import backend.umath
 
+def validate(state):
+    ret = backend.umath.payment(state)
+    state = dict(state)
+    for r in ret:
+        state[r.src] += r.value
+        state[r.dst] -= r.value
+    for k, v in state.items():
+        assert v == 0
+
 def test1():
     state = [
         ('u1',  40),
         ('u2', -35),
         ('u3', -50),
         ('u4',  60),
-        ('u5',  -5),
+        ('u5', -15),
     ]
-    ret = backend.umath.payment(state)
-    for i in ret:
-        print(i)
+    validate(state)
 
 
 if __name__ == '__main__':
