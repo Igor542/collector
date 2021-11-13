b531944302a71a4f931f350c5464698c0f5dc4ce
evatux <evatux@gmail.com>
2021-11-12 22:42:28 -0800

    src: be: umath: test: minor improvement

--- src/backend/test/test_umath.py
+++ src/backend/test/test_umath.py
@@ -8,8 +8,9 @@ SRC_DIR = os.path.dirname(os.path.dirname(TEST_DIR))
 sys.path.insert(0, SRC_DIR)
 import backend.umath
 
-def validate(state):
+def validate(state, length=None):
     ret = backend.umath.payment(state)
+    if length: assert len(ret) == length
     state = dict(state)
     for r in ret:
         state[r.src] += r.value
@@ -18,14 +19,20 @@ def validate(state):
         assert v == 0
 
 def test1():
-    state = [
+    validate([
+        ('u1', 0),
+        ('u2', 0),
+    ], 0)
+
+    validate([
         ('u1',  40),
         ('u2', -35),
         ('u3', -50),
         ('u4',  60),
         ('u5', -15),
-    ]
-    validate(state)
+    ])
+
+
 
 
 if __name__ == '__main__':
