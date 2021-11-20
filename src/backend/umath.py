f75f1eafb0016d643bfff1c6f1b6f808431c1f39
Igor Safonov <igorsafonov42@gmail.com>
2021-11-20 01:42:07 -0800

    src: style

--- src/backend/umath.py
+++ src/backend/umath.py
@@ -1,5 +1,4 @@
 from backend.utypes import PayOffItem
-
 """
 Input:
     - state: list of pairs(key, value)
@@ -8,11 +7,14 @@ Returns:
 Requirement:
     - sum(value) = 0
 """
+
+
 def payment(state):
     assert len(state) >= 1
-    state = [x for x in state] # copy state
+    state = [x for x in state]  # copy state
 
-    def state_sort(state): state.sort(key = lambda x: x[1])
+    def state_sort(state):
+        state.sort(key=lambda x: x[1])
 
     length = len(state)
 
@@ -20,17 +22,17 @@ def payment(state):
     while True:
         state_sort(state)
 
-        dept = - state[0][1]
+        dept = -state[0][1]
         if dept == 0: break
 
         for i in range(length):
             j = length - 1 - i
             if state[j][1] <= 0:
                 assert j + 1 < length
-                assert state[j+1][1] > 0
-                ret.append(PayOffItem(state[0][0], state[j+1][0], dept))
+                assert state[j + 1][1] > 0
+                ret.append(PayOffItem(state[0][0], state[j + 1][0], dept))
                 state[0] = (state[0][0], 0)
-                state[j+1] = (state[j+1][0], state[j+1][1] - dept)
+                state[j + 1] = (state[j + 1][0], state[j + 1][1] - dept)
                 state_sort(state)
                 break
             elif state[j][1] <= dept:
